aabf4c169724735513628d5ebcbe43b5ce8dd6d0
edy evan <edy_chuckie@hotmail.sg>
2018-11-06 01:20:39 +0700

    Add project files.

---
+++ ADMINKOMIKA/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ADMINKOMIKA.DAL;
+using ADMINKOMIKA.Models;
+using System.Web.Mvc;
+
+namespace ADMINKOMIKA.Controllers
+{
+    public class LoginController : Controller
+    {
+        private DALLogin DAL = new DALLogin();
+        // GET: Login
+        public ActionResult Index()
+        {
+            Session.Clear();
+            return View(new Login());
+        }
+
+        [HttpPost]
+        public ActionResult Index(Login get)
+        {
+            if (DAL.passwordistrue(get))
+            {
+                Session["UserName"] = get.username;
+                return RedirectToAction("DashBoard", "Home", new { });
+            }
+            else
+            {
+                return View(new Login() { validationtext = "Wrong Password" });
+            }
+        }
+
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login", new { });
+        }
+    }
+}
---
+++ ADMINKOMIKA/DAL/DALLogin.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ADMINKOMIKA.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ADMINKOMIKA.DAL
+{
+    public class DALLogin : Shared
+    {
+        public bool passwordistrue(Login get)
+        {
+            string SQL = "Select Count(*) from Adminlogin ";
+            SQL += "where username = '" + get.username + "' ";
+            SQL += "and password = '" + get.password + "'";
+            return (getint(SQL) > 0);
+        }
+    }
+}
